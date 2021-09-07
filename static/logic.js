37d7c9fe9b2292cf6fa7aaa8508245f56a8b63fc
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-09-07 14:14:08 +0800

    project created on August 7

---
+++ static/logic.js
@@ -0,0 +1,50 @@
+Moralis.initialize("W9CCwwg1nQQ1KFrqWYx8YY1nojrxTINwhMHLwYHD")// Application ID
+Moralis.serverUrl = "https://0iooker5q9ww.usemoralis.com:2053/server"// Server URL
+var web3;
+checkWeb3();
+
+function displayMessage(messageType, message){
+    messages = {
+        "00":`<div class= "alert alert-success"> ${message} </div>`,
+        "01":`<div class= "alert alert-danger"> ${message} </div>`
+    }
+    document.getElementById("resultSpace").innerHTML = messages[messageType];
+}
+
+async function checkWeb3(){
+    const ethereum = window.ethereum;
+    if(!ethereum || !ethereum.on) {
+        displayMessage("01", "This App Requires MetaMask, Please Install MetaMask");
+    }
+    else{
+        //displayMessage("00", "Metamask is Installed");
+        setWeb3Environment()
+    }
+}
+
+function setWeb3Environment(){
+    web3 = new Web3(window.ethereum);
+    getNetwork();
+    monitorNetwork();
+}
+
+async function getNetwork(){
+    chainID = await web3.eth.net.getId();
+    displayMessage("00","Active network is "+ getNetworkName(chainID));
+}
+
+function getNetworkName(chainID){
+    networks = {
+        1:"Ethereum Mainnet",
+        4:"Ethereum Rinkeby",
+        97:"Binance Smart Chain Testnet",
+        80001:"Polygon Mumbai Testnet"
+    }
+    return networks[chainID];
+}
+
+function monitorNetwork(){
+    Moralis.Web3.onChainChanged(function(){
+        window.location.reload()
+    })
+}
